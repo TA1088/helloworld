3393c5f2a0ecfb7045ef944329a242a8468c939b
TA1088 <zolsen8806@gmail.com>
2019-05-05 11:11:26 -0600

    Stopped before Basic Math Vid

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int myFavoriteNumber = 13;
+            float myFavFloat = 3.14f;
+            decimal myFaveDec = 100.01m;
         }
     }
 }
